9c4551006910b7b38b903734aeba6cd9f9b642d4
megan cho <meganlisacho@gmail.com>
2021-05-18 21:06:56 -0500

    linked dropdowns to data

--- index.js
+++ index.js
@@ -1,91 +1,85 @@
 // API KEY: wSlcKl1LSOpHXf164FTShbQZC4cmu1VWTJiipnFk
 
-const url = "https://developer.nps.gov/api/v1/parks?&api_key=wSlcKl1LSOpHXf164FTShbQZC4cmu1VWTJiipnFk"
+const url = "https://developer.nps.gov/api/v1/parks?&api_key=wSlcKl1LSOpHXf164FTShbQZC4cmu1VWTJiipnFk&limit=500"
 
-// let dropdown = document.getElementById('states-dropdown');
-// dropdown.length = 0;
 
-// let defaultOption = document.createElement('option');
-// defaultOption.text = 'Select a state';
+//NPS Dropdown
+let parksDrop = document.getElementById('nps-dropdown')
+parksDrop.length = 0
 
-// dropdown.add(defaultOption);
-// dropdown.selectedIndex = 0;
+let parksDropDefault = document.createElement('option')
+parksDropDefault.text = 'Select a national park'
 
-// fetch(url)  
-//   .then(  
-//     function(res) {  
-//       if (res.status !== 200) {  
-//         console.log('Looks like there was a problem. Status Code: ' + 
-//           res.status);  
-//         return;  
-//       }
+parksDrop.add(parksDropDefault)
+parksDrop.selectedIndex = 0
 
-//       // Examine the text in the response  
-//       res.json().then(function(nationalParks) {  
-//         let option;
+fetch(url)  
+  .then(  
+    function(res) {  
+      if (res.status !== 200) {  
+        console.log('Looks like there was a problem. Status Code: ' + 
+          res.status)
+        return
+      }
+
+      // Examine the text in the response  
+      res.json().then(function(nationalParks) {  
+        let option
     
-//     	for (let i = 0; i < nationalParks.data.length; i++) {
-//           option = document.createElement('option');
-//       	  option.text = nationalParks.data[i].states;
-//       	  option.value = nationalParks.data[i].states;
-//       	  dropdown.add(option);
-//     	}    
-//       });  
-//     }  
-//   )  
-//   .catch(function(err) {  
-//     console.error('Fetch Error -', err);  
-//   });
+    	for (let i = 0; i < nationalParks.data.length; i++) {
+          option = document.createElement('option')
+      	  option.text = nationalParks.data[i].fullName
+      	  option.value = nationalParks.data[i].fullName
+      	  parksDrop.add(option)
+    	}    
+      })
+    }  
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err)
+  })
 
+//Activities Dropdown
+let acsDrop = document.getElementById('activities-dropdown')
+acsDrop.length = 0
 
+let acsDropDefault = document.createElement('option')
+acsDropDefault.text = 'Select Activities'
 
+acsDrop.add(acsDropDefault)
+acsDrop.selectedIndex = 0
 
+fetch(url)  
+  .then(  
+    function(res) {  
+      if (res.status !== 200) {  
+        console.log('Looks like there was a problem. Status Code: ' + 
+          res.status)
+        return
+      }
 
+      // Examine the text in the response  
+      res.json().then(function(nationalParks) {  
+        let option
+    
+    	for (let i = 0; i < nationalParks.data.length; i++) {
+            for (let j = 0; j < nationalParks.data.length; j++) {
+                option = document.createElement('option')
+                option.text = nationalParks.data[i].activities[j].name
+                option.value = nationalParks.data[i].activities[j].name
+                acsDrop.add(option)
+            }
+    	}    
+      })
+    }  
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err)
+  })
 
-// // fetch(url)
-// // .then(function() {
+//THINGS TO WORK ON
+//CREATE FUNCTION FOR FETCH
+//CONNECT THE STATES TO THE PARKS TO THE ACTIVITIES
+//ADD IMAGES TO DISPLAY AND DESCRIPTION
+//SO MAYBE STATE -> ACTIVITIES -> PARK WITH IMAGE AND DESCRIPTION
 
-// })
-// .catch(function() {
-
-// });
-
-// function fetchParks() {
-//    return fetch(url)  
-//    .then(res => res.json())
-//    .then(json => renderParks(json))
-// }
-
-// function renderParks(parks) {
-//     const dropStates = document.querySelector('select')
-//     parks.forEach(park => {
-//         const h4 = document.createElement('h4')
-//         h4.innerHTML = park.name
-//         dropStates.appendChild(h4)
-//     })
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     fetchParks()
-// });
-      
-
-// // function fetchBooks() {
-// //     return fetch('https://anapioficeandfire.com/api/books')
-// //     .then(res => res.json())
-// //     .then(json => renderBooks(json))
-// //   }
-  
-// //   function renderBooks(books) {
-// //     const main = document.querySelector('main');
-// //     books.forEach(book => {
-// //       const h2 = document.createElement('h2');
-// //       h2.innerHTML = book.name;
-// //       main.appendChild(h2);
-// //     });
-// //   }
-  
-// //   document.addEventListener('DOMContentLoaded', function() {
-// //     fetchBooks();
-// //   });
-  
