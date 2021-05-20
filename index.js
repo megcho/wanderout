25ae9dd1e8dd92f03a4d2b2e2f61ad091a008b2f
megan cho <meganlisacho@gmail.com>
2021-05-20 18:35:38 -0500

    cleaned up code. assigned variable to fetch. created render functions.

--- index.js
+++ index.js
@@ -3,8 +3,23 @@
 // Assign API URL to a constant
 const url = 'https://developer.nps.gov/api/v1/parks?api_key=wSlcKl1LSOpHXf164FTShbQZC4cmu1VWTJiipnFk&limit=500'
 
+// Fetch
+let parks = null
+getParks()
 
-// Activities Dropdown
+function getParks(){
+	fetch(url)
+		.then(res => {
+			return res.json()
+		})
+		.then(data => {
+			parks = data
+			console.log(parks)
+            renderActivities()
+		})
+}
+
+// Render Activities Variable and Function
 const acsDrop = document.getElementById('activities-dropdown')
 acsDrop.length = 0
 
@@ -14,43 +29,39 @@ acsDropDefault.disabled = true
 
 acsDrop.add(acsDropDefault)
 acsDrop.selectedIndex = 0
-
-// Activites Fetch
-
-fetch(url)  
-  .then(res => res.json().then(parks => {  
+function renderActivities() {  
     let option
     for (let i = 0; i < parks.data.length; i++) {
+        for (let j = 0; j < parks.data.length; j++) {
             option = document.createElement('option')
-            option.text = parks.data[i].activities[0].name
-            option.value = parks.data[i].activities[0].name
-            acsDrop.append(option)
-    } 
-  })
-) 
-.catch(err => {  
-    console.error('Fetch Error', err)
-    })
+            option.text = parks.data[i].activities[j].name
+            option.value = parks.data[i].activities[j].name
+            acsDrop.add(option)
+        }
+    }    
+  }
 
+// Event Listener for HTML Loaded
+document.addEventListener('DOMContentLoaded', (e) => {
+    renderActivities()
+})
 
-// Click Event Listener and Activities Selected
+// Event Listener for Submit and Activities Selected
 const wanderBtn = document.getElementById('submit')
 
 const letsWander = wanderBtn.addEventListener('click', () => {
     const selectedState = document.getElementById('states-dropdown')
     const checkedActivity = document.querySelectorAll('#activities-dropdown :checked')
     const selected = [...checkedActivity, selectedState].map(option => option.value)
-    alert(selected)
+    console.log(selected)
+    renderParks()
 })
 
+// Render Parks Function
 const parkDiv = document.getElementById('park-results')
-const selectedState = document.getElementById('states-dropdown')
-const checkedActivity = document.querySelectorAll('#activities-dropdown :checked') 
 
-// Parks Fetch
 
-fetch(url)  
-  .then(res => res.json().then(parks => {  
+function renderParks() {
     for (let i = 0; i < parks.data.length; i++) {
             const ul = document.createElement('ul')
             const img = document.createElement('img')
@@ -60,5 +71,16 @@ fetch(url)
             descrip.innerText = parks.data[i].description
             parkDiv.append(ul, img, descrip)
         }  
-  })
-) 
+  }
+
+
+  
+
+
+
+
+
+// assign fetch data to a variable
+// use the variable in a function to populate dropdown
+// use the variable in a function to populate parks 
+// return the function to populate parks in the on click function
